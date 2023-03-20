ea8230544a8f3eadd4ae29c67c7c6ad2508f434c
Xsims <romeo.prosecco@gmail.com>
2023-03-20 21:49:15 +0100

    🚨 Suppress false positive warnings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,9 @@
+// "DSL_SCOPE_VIOLATION" suppress false positive errors on `libs` due to a gradle
+// issue: https://github.com/gradle/gradle/issues/22797.
+// "UnstableApiUsage" suppress warnings from code marked unstable due to the use
+// of experimental features annotated @Incubating.
+@file:Suppress("UnstableApiUsage")
+
 pluginManagement {
     includeBuild("build-logic")
     repositories {
